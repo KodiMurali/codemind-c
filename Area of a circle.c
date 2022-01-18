87b8d6f424c24d39bb2481fef61932b7462ac0cc
KodiMurali <97437638+KodiMurali@users.noreply.github.com>
2022-01-18 17:49:35 +0530

    #include <stdio.h> int main() {      int radius;      float area,pi=3.14;     scanf("%d",&radius);     area = pi*radius*radius;     printf("%0.2f ", area);
        return 0;  
    }

---
+++ Area of a circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>   
+int main()   
+{  
+     int radius;
+     float area,pi=3.14;  
+    scanf("%d",&radius);  
+    area = pi*radius*radius;  
+    printf("%0.2f
+", area);  
+    
+    return 0;  
+} 
