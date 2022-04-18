84b7e0175a6d184162a257bd716d37c674a2f776
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-04-18 22:23:00 +0530

    #include <stdio.h>
    int main()    
    {    
        int num;
        scanf("%d",&num);
        int rem = 0, sum = 0, n;    
        n = num;
        while(num > 0){    
            rem = num%10;    
            sum = sum + rem;    
            num = num/10;    
        }    
        if(n%sum == 0)    
            printf("True");    
        else    
            printf("False");    
         
        return 0;    
    }

--- Harshed_number.c
+++ Harshed_number.c
@@ -1,22 +1,20 @@
-#include<stdio.h>
-int main()
-{
-    int n,t,r,sum=0;
-    scanf("%d",&n);
-    t=n;
-    while(n>0)
-    {
-        r=n%10;
-        sum=sum+r;
-        n=n/10;
-    }
-    if(t%sum==0)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
-    return 0;
-}
+#include <stdio.h>    
+     
+int main()    
+{    
+    int num;
+    scanf("%d",&num);
+    int rem = 0, sum = 0, n;    
+    n = num;
+    while(num > 0){    
+        rem = num%10;    
+        sum = sum + rem;    
+        num = num/10;    
+    }    
+    if(n%sum == 0)    
+        printf("True");    
+    else    
+        printf("False");    
+     
+    return 0;    
+}  
