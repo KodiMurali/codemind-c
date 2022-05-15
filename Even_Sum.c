f7be617696557203b982125167902f9157386a33
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-05-15 07:48:00 +0530

    #include<stdio.h> main() {     int a[10],i,sum=0,n;     scanf("%d",&n);     for(i=0; i<n; i++)         scanf("%d",&a[i]);     for(i=0; i<n; i++)     {         if(a[i]%2==0)         {             sum=sum+a[i];         }     }     printf("%d",sum); }

---
+++ Even_Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int a[10],i,sum=0,n;
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+        scanf("%d",&a[i]);
+    for(i=0; i<n; i++)
+    {
+        if(a[i]%2==0)
+        {
+            sum=sum+a[i];
+        }
+    }
+    printf("%d",sum);
+}
